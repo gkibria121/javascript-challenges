f438dc7aed81b6ee18202cdf8f9ace92014ad725
gkibria <gkibria121@gmail.com>
2025-02-05 08:24:12 +0600

    practice singleton

--- src/singleton.ts
+++ src/singleton.ts
@@ -1,2 +1,19 @@
-console.log("hello world");
-console.log("hello the world");
+class AppConfig {
+  private static instance: AppConfig;
+  private constructor() {
+    //load config
+  }
+
+  public static getInstance() {
+    if (!AppConfig.instance) {
+      AppConfig.instance = new AppConfig();
+    }
+    return AppConfig.instance;
+  }
+}
+
+let conf1 = AppConfig.getInstance();
+let conf2 = AppConfig.getInstance();
+if (conf1 === conf2) {
+  console.log("this is singleton");
+}
