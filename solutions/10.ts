8f74fdf858c9e3c375fb8f08deaff65fd5af3c42
gkibria <gkibria121@gmail.com>
2025-02-06 20:49:34 +0600

    wip

---
+++ solutions/10.ts
@@ -0,0 +1,7 @@
+// **Secure sensitive data in local storage**
+// Store and retrieve sensitive data in local storage, ensuring the data is encrypted and secure.
+
+// **Techniques to Use:**
+
+// - `window.localStorage`
+// - AES encryption libraries like `crypto.subtle` (Web Crypto API)
