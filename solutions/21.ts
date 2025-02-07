ca21ab7d974d0bba6af36e740cb7c9db70e75c2b
gkibria <gkibria121@gmail.com>
2025-02-07 15:31:05 +0600

    solve 23

---
+++ solutions/21.ts
@@ -0,0 +1,8 @@
+// Convert the following chained promises into an `async/await` implementation:
+
+// ```javascript
+// fetchData()
+//   .then((data) => processData(data))
+//   .then((result) => saveData(result))
+//   .catch((err) => console.error(err));
+// ```
